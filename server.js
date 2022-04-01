86d9dcece0e8ae649a9970723b6fd27d7a4ed221
unknown <sabri.rakrouki@esprit.tn>
2022-04-01 19:53:17 +0200

    push apiDemo

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config()
+const express=require("express");
+const mongoose =require("mongoose")
+const cors = require("cors")
+const fileUpload= require("express-fileupload")
+const cookieParser=require("cookie-parser")
+
+const app= express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles:true
+}))
+//Routes
+app.use("/user",require('./routes/userRouter'))
+app.use("/api",require('./routes/categoryRouter'))
+app.use('/api',require('./routes/upload'))
+app.use('/api',require('./routes/productRouter'))
+
+
+
+
+
+const URI=process.env.MONGO_URL
+mongoose.connect(URI,{
+    
+    
+
+},err=>{
+    if(err)throw err;
+    console.log("connected to mongoDB")
+})
+
+
+app.get('/',(req,res)=>{
+    
+res.json({msg: "welcome my to firt tuto"})
+})
+const PORT=process.env.PORT|| 5000
+app.listen(PORT,()=>{
+    console.log('server is running on port ',PORT)
+} )
